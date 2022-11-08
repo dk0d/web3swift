47985436112c7a6590c9ad97ac2c8dd7202a575a
dk0d <dancapecci@gmail.com>
2022-11-08 12:40:58 -0500

    update gas resolver

--- Sources/web3swift/Web3/Web3+Resolver.swift
+++ Sources/web3swift/Web3/Web3+Resolver.swift
@@ -27,13 +27,10 @@ public class PolicyResolver {
         tx.gasLimit = try await resolveGasEstimate(for: tx, with: policies.gasLimitPolicy, api: provider.api)
 
         if case .eip1559 = tx.type {
-            tx.maxFeePerGas = await resolveGasBaseFee(for: policies.maxFeePerGasPolicy, api: provider.api)
             tx.maxPriorityFeePerGas = await resolveGasPriorityFee(for: policies.maxPriorityFeePerGasPolicy, api: provider.api)
-            if let priorityGas = tx.maxPriorityFeePerGas,
-               let feePerGas = tx.maxFeePerGas {
-                guard priorityGas < feePerGas else {
-                    throw Web3Error.processingError(desc: "Max priority fee per gas is higehr than max fee per gas")
-                }
+            if let priorityGas = tx.maxPriorityFeePerGas {
+                let baseFee = await resolveGasBaseFee(for: policies.maxFeePerGasPolicy, api: provider.api)
+                tx.maxFeePerGas = 2 * baseFee + priorityGas
             }
         } else {
             tx.gasPrice = await resolveGasPrice(for: policies.gasPricePolicy, api: provider.api)
